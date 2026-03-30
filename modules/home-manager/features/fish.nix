0fb5704ef68e5e94bf5dce38724154d1c244f5b1
TwistedZeon <twistedzeon@vivaldi.net>
2026-03-30 00:02:19 -0400

    Properly fixed syntax in fish..

--- modules/home-manager/features/fish.nix
+++ modules/home-manager/features/fish.nix
@@ -20,7 +20,7 @@
                   else
                       cd /home/zeon/udpbd-server/build && sudo ./udpbd-server.x86_64 /dev/sda2
                   end";
-           ps2-remount = "sudo mount -t exfat -o uid=1000,gid=1000 /dev/sda2 /mnt/PS2"
+           ps2-remount = "sudo mount -t exfat -o uid=1000,gid=1000 /dev/sda2 /mnt/PS2";
            
            dockerssh = "ssh root@192.168.0.194";
       };
