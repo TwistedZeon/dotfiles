92445d46b98ff7bcecf7efd28250a2a4a22d41a0
TwistedZeon <twistedzeon@vivaldi.net>
2026-03-29 23:59:40 -0400

    Updated functions for fish

--- modules/home-manager/features/fish.nix
+++ modules/home-manager/features/fish.nix
@@ -10,6 +10,19 @@
          functions = {
       
            server-u = "sudo mount -t nfs 192.168.0.205:/4TB /mnt/Server4TB";
+           
+           server-d = "sudo umount /mnt/Server4TB";
+
+           server2-u = "sudo mount -t nfs 192.168.0.205:/750GB /mnt/Server4TB";
+           
+           ps2 = "if grep -qs /mnt/PS2 /proc/mounts
+                       sudo umount /dev/sda2 && cd /home/zeon/udpbd-server/build && sudo ./udpbd-server.x86_64 /dev/sda2
+                  else
+                      cd /home/zeon/udpbd-server/build && sudo ./udpbd-server.x86_64 /dev/sda2
+                  end";
+           ps2-remount = "sudo mount -t exfat -o uid=1000,gid=1000 /dev/sda2 /mnt/PS2"
+           
+           dockerssh = "ssh root@192.168.0.194"
       };
     };
   };
