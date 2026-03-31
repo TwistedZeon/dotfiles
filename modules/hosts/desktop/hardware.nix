4384b04e9674e2595db5203db7affa5145f1bee6
TwistedZeon <8218882+TwistedZeon@users.noreply.github.com>
2026-03-31 03:43:51 -0400

    Updated desktop hardware to properly reflect the host and not vm

--- modules/hosts/desktop/hardware.nix
+++ modules/hosts/desktop/hardware.nix
@@ -1,31 +1,17 @@
 { self, inputs, ... }: {
   
-  flake.nixosModules.vmHardware = { config, lib, pkgs, modulesPath, ... }: {
-    imports =[ 
-      (modulesPath + "/profiles/qemu-guest.nix")
-    ];
+  flake.nixosModules.desktopHardware = { config, lib, pkgs, modulesPath, ... }: {
+    imports =
+      [ (modulesPath + "/installer/scan/not-detected.nix")
+      ];
 
-    boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
     boot.initrd.kernelModules = [ ];
     boot.kernelModules = [ "kvm-amd" ];
     boot.extraModulePackages = [ ];
-    
-    fileSystems."/" =
-      { device = "/dev/disk/by-uuid/daa4a070-f6d8-4063-a709-a9097c86515c";
-        fsType = "ext4";
-      };
-
-    fileSystems."/boot" =
-      { device = "/dev/disk/by-uuid/56E3-D64B";
-        fsType = "vfat";
-        options = [ "fmask=0077" "dmask=0077" ];
-      };
-
-    swapDevices =
-      [ { device = "/dev/disk/by-uuid/0ac5add8-d48b-4031-ad4a-44bfe01f5231"; }
-      ];
 
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
   
 }
