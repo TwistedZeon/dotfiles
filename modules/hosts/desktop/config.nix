527ca4b1325189366a032a9b943c2a4992d522dd
Zeon <twistedzeon@vivaldi.net>
2026-04-01 00:44:54 -0400

    Added pavucontrol

--- modules/hosts/desktop/config.nix
+++ modules/hosts/desktop/config.nix
@@ -118,6 +118,7 @@
     filezilla
     gimp
     libreoffice
+    pavucontrol
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by   default.
     #  wget
     ];
