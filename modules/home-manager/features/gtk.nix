6f9fde02cda091c18601a5e6cd1c98c5628c219f
Zeon <twistedzeon@vivaldi.net>
2026-07-25 04:46:48 -0400

    Swapped gtk to dark & papirus icons

--- modules/home-manager/features/gtk.nix
+++ modules/home-manager/features/gtk.nix
@@ -17,27 +17,50 @@
           package = pkgs.gnome-themes-extra;
         };
 
+        iconTheme = {
+          name = "Papirus-Dark";
+          package = pkgs.papirus-icon-theme;
+        };
+
         gtk3.extraConfig = {
-          gtk-application-prefer-dark-theme = false;
+          gtk-application-prefer-dark-theme = true;
         };
 
         gtk4.extraConfig = {
-          gtk-application-prefer-dark-theme = false;
+          gtk-application-prefer-dark-theme = true;
         };
 
         gtk4.theme = null;
       };
       dconf.settings = {
         "org/gnome/desktop/interface" = {
-          color-scheme = "prefer-light";
+          color-scheme = "prefer-dark";
         };
       };
-      home.file.".config/gtk-4.0/gtk.css".text = ''
-        window,
-        window.background,
-        .background {
-          border-radius: 0;
-        }
-      '';
+      home.file = {
+        ".config/gtk-3.0/gtk.css".text = ''
+          @define-color accent_color #DB4343;
+          @define-color theme_selected_bg_color #DB4343;
+          @define-color theme_selected_fg_color #FFFFFF;
+
+          /* Nemo file selection */
+          .view:selected,
+          .view:selected:focus {
+            background-color: #DB4343;
+            color: #FFFFFF;
+          }
+        '';
+
+        ".config/gtk-4.0/gtk.css".text = ''
+          window,
+          window.background,
+          .background {
+            border-radius: 0;
+          }
+
+          /* GTK 4 accent colour */
+          @define-color accent_color #DB4343;
+        '';
+      };
     };
 }
