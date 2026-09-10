7d5b9fd280fd45cb275f881fa1a654c31c184c8b
Zeon <twistedzeon@vivaldi.net>
2026-09-10 17:24:28 -0400

    Added outputs to Umbriel

--- modules/home-manager/features/umbriel/umbriel.nix
+++ modules/home-manager/features/umbriel/umbriel.nix
@@ -13,6 +13,16 @@
           general.autostart = [ "noctalia" ];
           layout.gap = 0;
           input.keyboard.layout = "us";
+          output.DP-1 = {
+               mode = "1920x1080@143.980";
+               position = [ 0 0 ];
+               scale = 1.0;
+             };
+          output.HDMI-A-1 = {
+             mode = "1440x900@74.984";
+             position = [ 1920 50 ];
+             scale = 1.0;
+           };
           keybinds = {
             "Mod+Return" = "spawn:ghostty";
             "Mod+Q" = "window-close";
